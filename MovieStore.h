bc2da147f9d0715d151e99ecd654ba53bd723db1
Cyrus Sarkosh <csarkosh@uw.edu>
2016-02-24 16:56:19 -0800

    Added files via upload
    Movie Store class which contains the Inventory System and a container of customers. Possibly may include Parser functions to read stored Movie, Customer, and Command data.

---
+++ MovieStore.h
@@ -0,0 +1,27 @@
+#ifndef MOVIESTORE_H
+#define MOVIESTORE_H
+
+#include <iostream>
+#include "Hashtable.h"
+
+
+namespace css343_group4
+{
+	class MovieStore
+	{
+	public:
+		//Constructors/Destructor
+		MovieStore();
+		~MovieStore();
+
+		//Parsers?  <---Possibly not needed
+		bool readCustomers(std::ifstream&);
+		bool readMovies(std::ifstream&);
+		bool readCommands(std::ifstream&);
+	private:
+		//HashTable<Customer> customers;	TODO: Add Person heirarchy
+		//InventorySystem* invSys;			TODO: Add Inventory System
+	};
+}
+
+#endif
