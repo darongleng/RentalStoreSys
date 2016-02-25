f9dd295bee4d76e2cfbfbc800bacd24155ee7bdd
darongleng <darongleng@gmail.com>
2016-02-24 16:48:01 -0800

    add movie

---
+++ Movie.h
@@ -0,0 +1,19 @@
+#include <string>
+
+class Movie {
+private:
+	string title;
+	string director;
+	int yearOfRelease;
+	int stock;
+public:
+	virtual void setTitle(string newTitle);
+	virtual void setDirector(string newDirector);
+	virtual void setYearOfRelease(int yearOfRelease);
+	virtual void setStock(int stock);
+
+	virtual int title() const;
+	virtual int director() const;
+	virtual int getYearOfRealse() const;
+	virtual int get stock() const;
+};
